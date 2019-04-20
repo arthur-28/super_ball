8963a69bd5d0651efe11523cbebcad59229ffdd5
Arthur <arthur-korol@mail.ru>
2019-04-20 12:00:35 +0700

    Закоментирована попытка реализации меню

--- main_sb.py
+++ main_sb.py
@@ -13,43 +13,19 @@ bmp_background = arcade.load_texture('img/1 lvl.jpg')
 bmp_platform = arcade.load_texture('img/platform1.png')
 bmp_ball = arcade.load_texture('img/ball.png')
 
-from tkinter import *
+# from tkinter import *
 
-root = Tk()
-root.title("GUI на Python")
-root.geometry("800x600")
+# root = Tk()
+# root.title("GUI на Python")
+# root.geometry("800x600")
 
-main_menu = Menu()
-main_menu.add_cascade(label="File")
-main_menu.add_cascade(label="Edit")
-main_menu.add_cascade(label="View")
-
-root.config(menu=main_menu)
-root.mainloop()
-
-from tkinter import *
-
-root = Tk()
-root.title("GUI на Python")
-root.geometry("800x600")
-
-main_menu = Menu()
-
-file_menu = Menu()
-file_menu.add_command(label="New")
-file_menu.add_command(label="Save")
-file_menu.add_command(label="Open")
-file_menu.add_separator()
-file_menu.add_command(label="Exit")
-
-main_menu.add_cascade(label="File", menu=file_menu)
-main_menu.add_cascade(label="Edit")
-main_menu.add_cascade(label="View")
-
-root.config(menu=main_menu)
-
-root.mainloop()
-
+# main_menu = Menu()
+# main_menu.add_cascade(label="File")
+# main_menu.add_cascade(label="Edit")
+# main_menu.add_cascade(label="View")
+#
+# root.config(menu=main_menu)
+# root.mainloop()
 
 
 class Platform:
